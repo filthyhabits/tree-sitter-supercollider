0a71427f215aada6e7f7b2e8370110ba3ad522cd
David Kiers <dgkiers@gmail.com>
2025-09-05 09:29:27 +0200

    Add the newly generated src/ files

--- src/scanner.c
+++ src/scanner.c
@@ -1,12 +1,8 @@
-#include "tree_sitter/parser.h"
+#include <tree_sitter/parser.h>
 #include <wctype.h>
 // Block comment stuff here is largely nicked from tree-sitter-rust https://github.com/tree-sitter/tree-sitter-rust
 
-enum TokenType { 
-  BLOCK_COMMENT,
-  // STRING,
-  SPACE_SEPARATOR
-};
+enum TokenType { BLOCK_COMMENT, STRING };
 
 void *tree_sitter_supercollider_external_scanner_create() { return NULL; }
 void tree_sitter_supercollider_external_scanner_destroy(void *p) {}
@@ -19,17 +15,7 @@ static void advance(TSLexer *lexer) { lexer->advance(lexer, false); }
 static bool is_num_char(int32_t c) { return c == '_' || iswdigit(c); }
 
 bool tree_sitter_supercollider_external_scanner_scan(
-  void *payload, TSLexer *lexer, const bool *valid_symbols) {
-
-  // space as separator in '{ |arg1 arg2| }' style argument declaration
-  if (valid_symbols[SPACE_SEPARATOR] && iswspace(lexer->lookahead)) {
-    while (iswspace(lexer->lookahead))
-      lexer->advance(lexer, true);
-    if (lexer->lookahead != '=' && lexer->lookahead != '|') {
-      lexer->result_symbol = SPACE_SEPARATOR;
-      return true;
-    }
-  }
+    void *payload, TSLexer *lexer, const bool *valid_symbols) {
 
   while (iswspace(lexer->lookahead))
     lexer->advance(lexer, true);
